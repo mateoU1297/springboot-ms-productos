2078e9975317917ef7583752022c8fb276cd93e5
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-06-27 13:03:06 -0500

    se crea el repositorio

---
+++ src/main/java/com/springboot/app/msproductos/SpringbootMsProductosApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.msproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMsProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMsProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/app/msproductos/SpringbootMsProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.msproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMsProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
