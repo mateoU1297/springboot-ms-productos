2804c474b9c5a1fc548bd3a239a270bb78068d74
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-06-29 18:11:42 -0500

    se arregla bug

--- src/main/java/com/springboot/app/msproductos/controllers/ProductoController.java
+++ src/main/java/com/springboot/app/msproductos/controllers/ProductoController.java
@@ -1,6 +1,10 @@
 package com.springboot.app.msproductos.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,19 +14,27 @@ import com.springboot.app.msproductos.models.service.IProductoService;
 
 @RestController
 public class ProductoController {
-		
-		@Autowired
-		private IProductoService productoService;
-		
-		@GetMapping("/listar")
-		public java.util.List<Producto> listar(){
-			return productoService.findAll();
-		}
-		
-		@GetMapping("/ver/{id}")
-		public Producto detalle(@PathVariable Long id) {
-			Producto producto = productoService.findById(id);
+
+	@Autowired
+	private Environment env;
+
+	@Autowired
+	private IProductoService productoService;
+
+	@GetMapping("/listar")
+	public List<Producto> listar() {
+		return productoService.findAll().stream().map(producto -> {
+			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 			return producto;
-		}
+		}).collect(Collectors.toList());
+	}
+
+	@GetMapping("/ver/{id}")
+	public Producto detalle(@PathVariable Long id) {
+		Producto producto = productoService.findById(id);
+		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+
+		return producto;
+	}
 
 }
