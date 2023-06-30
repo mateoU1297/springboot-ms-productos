8cf8321e237efd815cc4a9c0053da785303af9ca
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-06-30 18:41:59 -0500

    se agrega metodos faltantes del CRUD

--- src/main/java/com/springboot/app/msproductos/models/service/ProductoServiceImpl.java
+++ src/main/java/com/springboot/app/msproductos/models/service/ProductoServiceImpl.java
@@ -27,5 +27,16 @@ public class ProductoServiceImpl implements IProductoService {
 	public Producto findById(Long id) {
 		return productoDao.findById(id).orElse(null);
 	}
+
+	@Override
+	@Transactional
+	public Producto save(Producto producto) {
+		return productoDao.save(producto);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		productoDao.deleteById(id);
+	}
 	
 }
